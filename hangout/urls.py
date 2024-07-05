fa72962732f8d7351fc7fd3879fa1781e0491183
sin yo han <newjohn0518@naver.com>
2024-07-05 13:19:54 +0900

    image

--- hangout/urls.py
+++ hangout/urls.py
@@ -22,13 +22,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from user_post.views import home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('user_post.urls')),
     path('accounts/', include('accounts.urls')),  # Include accounts app URLs
-  path('', TemplateView.as_view(template_name='index.html')),
+  path('', home, name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user_post/views.py
+++ user_post/views.py
@@ -4,3 +4,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'user_post/post_list.html', {'posts': posts})
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
